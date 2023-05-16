5b18ab2c2a50c256707f82b61f29391e06905025
dorahim87 <abderrahim.douakha@gmail.com>
2023-05-16 14:40:44 -0400

    crud de bouteille et ajustement du cellier crud

--- backend/app/Models/BouteilleCellier.php
+++ backend/app/Models/BouteilleCellier.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class BouteilleCellier extends Model
 {
     protected $table = 'bouteilles_celliers';
-    protected $fillable = ['millesime', 'quantite','date_achat','garde_jusqua','note','cellier_id','bouteille_id'];
+    protected $fillable = ['millesime', 'note','cellier_id','bouteille_id'];
 
     public function bouteille()
     {
