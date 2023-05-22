19ff144da3bd54270ae202540445020062ff4992
Chengfc <chenf8787@gmail.com>
2023-05-22 14:13:50 -0400

    ajouts des dossiers/fichiers composantes de cellier - vino2-m21 fc

--- frontend/src/app/cellier/liste-cellier/liste-cellier.component.ts
+++ frontend/src/app/cellier/liste-cellier/liste-cellier.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {AuthService} from "../../auth/auth-service";
+import {MatDialog} from "@angular/material/dialog";
+import {SupprimerCellierComponent} from "../supprimer-cellier/supprimer-cellier.component";
 
 @Component({
   selector: 'app-liste-cellier',
@@ -6,5 +11,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./liste-cellier.component.scss']
 })
 export class ListeCellierComponent {
+  private urlBackend: string = 'http://127.0.0.1:8000/api/celliers';
+  ListeCelliers: any[] = [];
 
+  constructor(private http: HttpClient, private router: Router, private authService: AuthService, public dialog: MatDialog) {
+  }
+
+  ngOnInit(): void {
+    this.getAllCelliers()
+  }
+
+  getAllCelliers () {
+    let httpOption = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'Authorization': "Bearer " + this.authService.getBearerToken()
+      })
+    };
+      this.http.get(this.urlBackend, httpOption).subscribe((results: any) => {
+        this.ListeCelliers = results
+      });
+  }
+
+  openDialog(id: bigint, nom: string): void {
+    const dialogRef = this.dialog.open(SupprimerCellierComponent, {
+      data: {id: id, nom: nom},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.getAllCelliers()
+    });
+  }
 }
--- frontend/src/app/cellier/modifier-cellier/modifier-cellier.component.ts
+++ frontend/src/app/cellier/modifier-cellier/modifier-cellier.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {catchError} from "rxjs/operators";
+import {EMPTY} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {AuthService} from "../../auth/auth-service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-modifier-cellier',
@@ -6,5 +12,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./modifier-cellier.component.scss']
 })
 export class ModifierCellierComponent {
+  public urlBackend: string = 'http://127.0.0.1:8000/api';
+  modifierCellier = new FormGroup({
+    nom: new FormControl('', Validators.required)
+  });
+  nom: any ;
+  id: any;
 
+  constructor(private http: HttpClient, private authService: AuthService, private router: Router, private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.http.get<any>(this.urlBackend + '/celliers/' + this.id )
+      .subscribe((result: any) => {
+        this.nom = result.nom
+      });
+  }
+
+  onSubmit() {
+    this.http.put<any>(this.urlBackend + '/celliers/' + this.id, this.modifierCellier.value)
+      .pipe(
+        catchError(error => {
+          return EMPTY;
+        })
+      )
+      .subscribe((result: any) => {
+        this.router.navigate(['/cellier'])
+      });
+  }
 }
