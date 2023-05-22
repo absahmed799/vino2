ee315c4a02e4360b4356583b63bd01e11047c405
Chengfc <chenf8787@gmail.com>
2023-05-22 14:11:25 -0400

    ajouts des dossiers/ fichiers composantes de layout - vino2-m21 fc

---
+++ frontend/src/app/layout/entete-auth/entete-auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entete-auth',
+  templateUrl: './entete-auth.component.html',
+  styleUrls: ['./entete-auth.component.scss']
+})
+export class EnteteAuthComponent {
+
+}
